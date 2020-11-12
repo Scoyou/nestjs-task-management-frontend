77bc2359a596bc75b7545762469d244229e5114b
Scott Young <syoung@instructure.com>
2020-11-11 20:32:18 -0700

    status dropdown working

--- components/TaskStatusDropdown.js
+++ components/TaskStatusDropdown.js
@@ -1,36 +1,43 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 
-const updateTaskStatus = async (id, taskStatus) => {
-  const res = await axios({
-    method: "patch",
-    url: `http://localhost:3001/tasks/${id}/status`,
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJpYXQiOjE2MDUxMzAxODMsImV4cCI6MTYwNTczNDk4M30.xR581PfgGwYPVnpxhFbKB2Stx0z6Um-ofJ_3h-6DIHA`,
-    },
-    data: {
-      status: taskStatus,
-    },
-  });
-};
+
 
 const TaskStatusDropdown = ({ task }) => {
+    const [status, setStatus] = useState(task.status);
   const options = [
     { key: 1, label: "OPEN", value: "OPEN" },
     { key: 2, label: "IN PROGRESS", value: "IN_PROGRESS" },
     { key: 3, label: "DONE", value: "DONE" },
   ];
 
+  const updateTaskStatus = async (id, taskStatus) => {
+    const res = await axios({
+      method: "patch",
+      url: `http://localhost:3001/tasks/${id}/status`,
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJpYXQiOjE2MDUxMzAxODMsImV4cCI6MTYwNTczNDk4M30.xR581PfgGwYPVnpxhFbKB2Stx0z6Um-ofJ_3h-6DIHA`,
+      },
+      data: {
+        status: taskStatus,
+      },
+    }).then(res => setStatus(res.data.status));
+  };
+
   const handleChange = (taskId) => (e) => {
     updateTaskStatus(taskId, e.value);
   };
 
+//   useEffect(() => {
+//     setStatus(task.status)
+//   }, [setStatus]);
+
   return (
     <div>
       <Select
-        value={task.status}
-        placeholder={task.status}
+        value={status}
+        placeholder={status}
         options={options}
         onChange={handleChange(task.id)}
       />
