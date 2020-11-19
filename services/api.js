edc590ec08239b60d7e7205910aaa62586b04dfb
Scott Young <syoung@instructure.com>
2020-11-19 16:24:26 -0700

    push to prod

--- services/api.js
+++ services/api.js
@@ -2,6 +2,7 @@ import Axios from "axios";
 
 let urls = {
     development: 'http://localhost:3001/',
+    production: 'https://task-management-backend-sy.herokuapp.com/'
 }
 const api = Axios.create({
     baseURL: urls[process.env.NODE_ENV],
